84c4ef02fb15b8d920bfc7755ecf7c74c94982a4
Rodrigo <rodrigomamani745@gmail.com>
2025-07-18 12:17:11 -0300

    Actualización de la BD

--- src/bd/flask_login.sql
+++ src/bd/flask_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-04-2025 a las 22:06:56
+-- Tiempo de generación: 18-07-2025 a las 17:13:33
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -42,7 +42,13 @@ CREATE TABLE `clientes` (
 
 INSERT INTO `clientes` (`id_clientes`, `nombre`, `email`, `telefono`, `existe`, `id_usuario`) VALUES
 (1, 'Mauricio', 'mauri00@gmail.com', '1144663372', 1, 1),
-(2, 'Mauricio', 'mauri00@gmail.com', '1144663372', 0, NULL);
+(2, 'Mauricio', 'mauri00@gmail.com', '1144663372', 0, NULL),
+(3, 'Federico', 'fede_25@outlook.com', '1144467324', 1, 5),
+(4, 'Maxi', 'maxi22@gmail.com', '1156748624', 1, 5),
+(5, 'Cliente3usuario', 'cliente3@hotmail.com', '1154435674', 1, 5),
+(6, 'Cliente1pablo', 'cliente1pablo@gmail.com', '1133625478', 1, 2),
+(7, 'Cliente2pablo', 'cliente2pablo@hotmail.com', '1145738634', 1, 2),
+(8, 'ClientedeUsuario3prueba', 'clienteu3@outlook.com', '', 1, 6);
 
 -- --------------------------------------------------------
 
@@ -66,7 +72,13 @@ CREATE TABLE `reparaciones` (
 --
 
 INSERT INTO `reparaciones` (`id_reparaciones`, `fecha_reparacion`, `descripcion`, `costo`, `existe`, `estado`, `id_clientes`, `id_usuarios`) VALUES
-(2, '2025-02-24', 'Reparación  de flex de netbook.', '50000', 1, 'pendiente', 1, 2);
+(2, '2025-02-24', 'Reparación  de flex de netbook.', '50000', 1, 'pendiente', 1, 1),
+(4, '2025-06-01', 'Cambio placa madre de Notebook (quemada).', '120000', 1, 'finalizado', 3, 5),
+(5, '2025-06-02', 'Cambio de procesador y ram mas limpieza.', '90000', 1, 'pendiente', 4, 5),
+(6, '2025-06-10', 'Limpieza completa, cambio de pasta cpu y gpu.', '90000', 1, 'Cancelado', 5, 5),
+(7, '2025-07-17', 'A cliente1pablo se le hizo una clonación de disco, de hdd a ssd.', '80000', 1, 'Pendiente', 6, 2),
+(8, '2025-07-17', 'Estado de prueba para verificar Existencia y select Estado.', '10000', 1, 'Finalizado', 6, 2),
+(9, '2025-07-17', 'Prueba3', '90000', 1, 'Pendiente', 8, 6);
 
 -- --------------------------------------------------------
 
@@ -91,7 +103,10 @@ INSERT INTO `usuarios` (`id_usuarios`, `nombre`, `contrasena`, `admin`, `existe`
 (1, 'rodris', 'admin', 1, 1, NULL),
 (2, 'Pablo', 'pablo', 0, 1, 'pablo2025@gmail.com'),
 (3, 'Pablo', 'pablo', 0, 0, 'pablo2025@gmail.com'),
-(4, 'Rodrigo', 'df', 0, 1, 'mamani.rodrigo.flores@gmail.co');
+(4, 'Rodrigo', 'df', 0, 1, 'mmf@gmail.com'),
+(5, 'usuario', 'usuario', 0, 1, NULL),
+(6, 'Usuario3prueba', 'usuario3prueba', 0, 1, 'usuario3@hotmail.com'),
+(7, 'asd', 'asdasda', 0, 1, 'asdasd@gmail.com');
 
 --
 -- Índices para tablas volcadas
@@ -126,19 +141,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_clientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_clientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `reparaciones`
 --
 ALTER TABLE `reparaciones`
-  MODIFY `id_reparaciones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_reparaciones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
